666a423c0d024b8fb6aee2084a35a64bc60e51b6
Sephiroth018 <Sephiroth018@users.noreply.github.com>
2021-04-21 23:42:11 +0200

    added import of data exported from https://mhrise.wiki-db.com/sim/?hl=en

--- MHRTalismanManager/Shared/SlotType.cs
+++ MHRTalismanManager/Shared/SlotType.cs
@@ -4,9 +4,9 @@ namespace MHRTalismanManager.Shared
 {
     public enum SlotType
     {
-        [Display(Name = "-")] None,
-        [Display(Name = "1")] Level1,
-        [Display(Name = "2")] Level2,
-        [Display(Name = "3")] Level3,
+        [Display(Name = "-")] None = 0,
+        [Display(Name = "1")] Level1 = 1,
+        [Display(Name = "2")] Level2 = 2,
+        [Display(Name = "3")] Level3 = 3,
     }
 }
