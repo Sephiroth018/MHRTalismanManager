610623ec74ccf53c667d478f484ba58d558decc3
Sephiroth018 <Sephiroth018@users.noreply.github.com>
2021-04-23 20:28:48 +0200

    refactoring

--- MHRTalismanManager/Server/Controllers/ImageRecognitionController.cs
+++ MHRTalismanManager/Server/Controllers/ImageRecognitionController.cs
@@ -7,17 +7,17 @@ namespace MHRTalismanManager.Server.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class OcrController : ControllerBase
+    public class ImageRecognitionController : ControllerBase
     {
         private readonly DataExtractionService _dataExtractionService;
 
-        public OcrController(DataExtractionService dataExtractionService)
+        public ImageRecognitionController(DataExtractionService dataExtractionService)
         {
             _dataExtractionService = dataExtractionService;
         }
 
         [HttpPost]
-        public async Task<TalismanDto> DoOcr()
+        public async Task<TalismanDto> ExtractDataFromImage()
         {
             await using var stream = Request.Body;
             return await _dataExtractionService.ExtractFromImage(stream);
